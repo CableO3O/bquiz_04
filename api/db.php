df0d2549b5fbe439780c9579a0f775f9bfc241e4
cable <cable870526@gmail.com>
2024-01-22 10:25:03 +0800

    db、前後台引入葉面

--- api/db.php
+++ api/db.php
@@ -142,22 +142,6 @@ function to($url){
 }
 
 
-$Total=new DB('total');
-$User=new DB('user');
-$News=new DB('news');
-$Que=new DB('que');
-$Log=new DB('log');
-
-if(!isset($_SESSION['visited'])){
-    if($Total->count(['date'=>date('Y-m-d')])>0){
-        $total=$Total->find(['date'=>date('Y-m-d')]);
-        $total['total']++;
-        $Total->save($total);
-    }else{
-        $Total->save(['total'=>1,'date'=>date('Y-m-d')]);
-    }
-    $_SESSION['visited']=1;
-}
 
 
 ?>
