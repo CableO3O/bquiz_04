4d4cc5d6bc83621c66d4a0ed39bdd2835c41bde6
cable <cable870526@gmail.com>
2024-02-16 13:46:22 +0800

    美化商品介面

--- front/good.php
+++ front/good.php
@@ -18,9 +18,41 @@
     }
 ?>
 <h1><?=$nav;?></h1>
+<style>
+    .item{
+        width: 80%;
+        height: 160px;
+        background:#EFCA85;
+        margin: 5px;
+        display: flex;
+        margin: 5px auto;
+    }
+    .item .img{
+        width: 33%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: 1px solid #999;
+    }
+    .item .info{
+        width: 67%;
+        display: flex;
+        flex-direction: column;
+    }
+    .info div{
+        border: 1px solid #999;
+        border-left: 0px;
+        border-top: 0px;
+        flex-grow: 1;
+    }
+    .info div:nth-child(1){
+        border-top: 1px solid #999;
+    }
+</style>
 <?php
 foreach ($goods as $good) {
 ?>
+<!-- table版 -->
 <!-- <table style="width: 100%;">
     <tr style="text-align:center;">
         <td rowspan="4"  class="pp" style="width: 40%;"><img style="width:200px;height:150px;margin-top:20px;margin-bottom:20px;" src="./img/<?=$good['img'];?>" alt=""></td>
@@ -41,11 +73,12 @@ foreach ($goods as $good) {
         <td class="pp">簡介:<?=mb_substr($good['intro'],0,30);?>...</td>
     </tr>
 </table> -->
+<!-- div版 -->
 <div class="item">
     <div class="img">
-        <img src="./img/<?=$good['img'];?>" alt="" style="width: 200px;height:150px">
+        <img src="./img/<?=$good['img'];?>" alt="" style="width: 80%;height:130px">
     </div>
-    <div class="imfo">
+    <div class="info">
         <div class="ct tt"><?=$good['name'];?></div>
         <div>價錢:<?=$good['price'];?></div>
         <div>規格:<?=$good['spec'];?></div>
