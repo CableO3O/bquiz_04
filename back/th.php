e7fac318616f4f8447bfe2651e56fb380d86bc9e
cable <cable870526@gmail.com>
2024-02-16 08:40:02 +0800

    完成後台商品列表顯示

--- back/th.php
+++ back/th.php
@@ -92,11 +92,15 @@
         <td>狀態</td>
         <td>操作</td>
     </tr>
+    <?php
+$goods=$Goods->all();
+foreach ($goods as $good) {
+    ?>
     <tr class="pp">
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td><?=$good['no'];?></td>
+        <td><?=$good['name'];?></td>
+        <td><?=$good['stock'];?></td>
+        <td><?=($good['sh']==1)?"上架":"下架";?></td>
         <td>
             <button>修改</button>
             <button>刪除</button>
@@ -104,4 +108,7 @@
             <button>下架</button>
         </td>
     </tr>
+    <?php
+}
+?>
 </table>
