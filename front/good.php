f2803a54073c4c8e289df308afd675ef83872014
cable <cable870526@gmail.com>
2024-02-16 11:30:28 +0800

    完成選單商品分類文字

---
+++ front/good.php
@@ -0,0 +1,17 @@
+
+    <?php
+    $type=$_GET['type']??0;
+    $nav='';
+    if ($type==0) {
+        $nav="全部商品";
+    }else{
+        $t=$Type->find($type);
+        if ($t['big_id']==0) {
+            $nav=$t['name'];
+        }else{
+            $b=$Type->find($t['big_id']);
+            $nav=$b['name'].">".$t['name'];
+        }
+    }
+?>
+<h1><?=$nav;?></h1>
